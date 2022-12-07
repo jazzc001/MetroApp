d0108dfbbee5ca9d0438199e20d1cf38bd2f785b
Yusra Tahir <yusra-tahir@hotmail.co.uk>
2022-12-07 15:52:41 +0000

    MVC Skeleton

---
+++ UserJourneyMVC/src/main/java/com/groupone/client/UserJourneyMvcApplication.java
@@ -0,0 +1,13 @@
+package com.groupone.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserJourneyMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserJourneyMvcApplication.class, args);
+	}
+
+}
