7ba42fa63445c0eb5d5a2ff8628372fc4f9fdc9f
jazzc001 <jazzcheung212@gmail.com>
2022-12-07 15:27:34 +0000

    added filepath

--- userservice/src/main/java/com/groupone/userservice/app/ServletInitializer.java
+++ userservice/src/main/java/com/groupone/userservice/app/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.groupone.userservice;
+package com.groupone.userservice.app;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
