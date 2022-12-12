8af373c86ab3618c79b5761484467d98499bb179
Amal Khan <Amalkhan1510@gmail.com>
2022-12-12 14:39:36 +0000

    Added login

---
+++ UserJourneyMVC/src/main/java/com/groupone/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.groupone.Controller;
+
+import com.groupone.entity.User;
+import com.groupone.service.UserJourneyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserJourneyService userJourneyService;
+
+    //-------------------------------------------------------------------LOGIN CONTROLLER------------------------------------------------------------------------
+    @RequestMapping("/")
+    public ModelAndView loginPageController() {
+        return new ModelAndView("login");
+    }
+    @RequestMapping("/login")
+    public ModelAndView loginController(@RequestParam("email") String email, @RequestParam("password") String password) {
+
+        // instantiate empty/blank MAV object
+        ModelAndView modelAndView = new ModelAndView();
+
+        // if login is correct
+        if (userJourneyService.login(email, password)) {
+            modelAndView.setViewName("Dashboard");
+        }
+        // if login check is failed
+        else {
+            // display message on selected view
+            modelAndView.addObject("message", "Unable to Login, either your password or email is incorrect.");
+            // show the Login page on the web app
+            modelAndView.setViewName("login");
+        }
+        return modelAndView;
+    }
+
+
+    }
